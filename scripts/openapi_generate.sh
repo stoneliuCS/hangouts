38b031bc679c998bcea1751ab80a099e9d92cccb
Stone Liu <liu.sto@northeastern.edu>
2025-06-10 19:46:47 -0400

    Update some dependencies

--- scripts/openapi_generate.sh
+++ scripts/openapi_generate.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_PATH=$(realpath "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
-OAS_SCRIPT_DIR=$SCRIPT_DIR/../api
+OAS_SCRIPT_DIR=$SCRIPT_DIR/../api-docs
 cd $OAS_SCRIPT_DIR
 bun run index.ts
 
