0db2492a393f4bae5b60f95232352cadeb26814e
Stone Liu <liu.sto@northeastern.edu>
2025-06-11 18:40:06 -0400

    Add better builds

--- hangouts/scripts/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SCRIPT_PATH=$(realpath "$0")
-SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
-SERVER_DIR=$SCRIPT_DIR/../
-cd $SERVER_DIR 
-go run ./cmd/server/main.go
--- scripts/openapi_generate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-SCRIPT_PATH=$(realpath "$0")
-SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
-
-OAS_SCRIPT_DIR=$SCRIPT_DIR/../api-docs
-cd $OAS_SCRIPT_DIR
-bun run index.ts
